5c4404d243e7a44bf0ab1cd71b1e3f72a8f78665
Misha Shkurat <misha@Misha-Shkurats-MacBook-Pro.local>
2013-03-12 17:02:14 +0200

    settings changed

--- workshopstore/settings.py
+++ workshopstore/settings.py
@@ -63,7 +63,7 @@ USE_TZ = True
 if PRODUCTION:
 	# Absolute filesystem path to the directory that will hold user-uploaded files.
 	# Example: "/home/media/media.lawrence.com/media/"
-	MEDIA_ROOT = '/web/http/workshopstore.com.ua/workshopstore/media/'
+	MEDIA_ROOT = '/web/http/workshopstore.com.ua/wss/media/'
 	
 	# URL that handles the media served from MEDIA_ROOT. Make sure to use a
 	# trailing slash.
@@ -74,7 +74,7 @@ if PRODUCTION:
 	# Don't put anything in this directory yourself; store your static files
 	# in apps' "static/" subdirectories and in STATICFILES_DIRS.
 	# Example: "/home/media/media.lawrence.com/static/"
-	STATIC_ROOT = '/web/http/workshopstore.com.ua/workshopstore/static/'
+	STATIC_ROOT = '/web/http/workshopstore.com.ua/wss/static/'
 	
 	# URL prefix for static files.
 	# Example: "http://media.lawrence.com/static/"
@@ -85,7 +85,7 @@ if PRODUCTION:
 	    # Put strings here, like "/home/html/static" or "C:/www/django/static".
 	    # Always use forward slashes, even on Windows.
 	    # Don't forget to use absolute paths, not relative paths.
-	    '/web/http/workshopstore.com.ua/workshopstore/products/static/'
+	    '/web/http/workshopstore.com.ua/wss/products/static/'
 	)
 else:
 	# Absolute filesystem path to the directory that will hold user-uploaded files.
