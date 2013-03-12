2e828455d10ee52a225b5d198e04c8f468d2421d
Misha Shkurat <misha@Misha-Shkurats-MacBook-Pro.local>
2013-03-12 17:13:00 +0200

    settings changed

--- workshopstore/settings.py
+++ workshopstore/settings.py
@@ -68,7 +68,7 @@ if PRODUCTION:
 	# URL that handles the media served from MEDIA_ROOT. Make sure to use a
 	# trailing slash.
 	# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-	MEDIA_URL = '/media/'
+	MEDIA_URL = 'http://test.workshopstore.com.ua/media/'
 	
 	# Absolute path to the directory static files should be collected to.
 	# Don't put anything in this directory yourself; store your static files
@@ -78,7 +78,7 @@ if PRODUCTION:
 	
 	# URL prefix for static files.
 	# Example: "http://media.lawrence.com/static/"
-	STATIC_URL = '/static/'
+	STATIC_URL = 'http://test.workshopstore.com.ua/static/'
 	
 	# Additional locations of static files
 	STATICFILES_DIRS = (
@@ -151,7 +151,7 @@ WSGI_APPLICATION = 'workshopstore.wsgi.application'
 
 if PRODUCTION:
     TEMPLATE_DIRS = (
-    '/web/http/workshopstore.com.ua/workshopstore/workshopstore/templates'
+    '/web/http/workshopstore.com.ua/wss/workshopstore/templates'
     )
     
     INSTALLED_APPS = (
